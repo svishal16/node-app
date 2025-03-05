9a0cb28a9aea5668e535f4812b940868f2793c5c
svishal16 <shrivastavavishal640@gmail.com>
2025-03-05 20:30:23 +0530

    alias var edit

--- cert_mgmt/export.sh
+++ cert_mgmt/export.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Export the certificate to a .cer file
-keytool -exportcert -keystore $KEYSTORE_DIR/$KEYSTORE -alias $CERT_ALIAS -file $PKCS_KEYSTORE_DIR/$PKCS_KEYSTORE/$CERT_ALIAS -storepass $STOREPASS
+keytool -exportcert -keystore $KEYSTORE_DIR/$KEYSTORE -alias $CERT_ALIAS -file "${PKCS_KEYSTORE_DIR}/${CERT_ALIAS}.cer" -storepass $STOREPASS
 
 # Export the private key from the keystore to a PKCS#12 (.p12) file
 keytool -importkeystore -srckeystore $KEYSTORE_DIR/$KEYSTORE -srcstorepass $STOREPASS -destkeystore $PKCS_KEYSTORE_DIR/$PKCS_KEYSTORE -deststoretype PKCS12 -srcalias $CERT_ALIAS -destkeypass $STOREPASS
